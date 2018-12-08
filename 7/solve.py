a978612c617525c23530fe904f8c5c97fdc5fb55
Hunsley.IO Ltd <alex.hunsley@gmail.com>
2018-12-08 19:44:11 +0000

    Put part 1 into a function, make part 2 placeholder

--- 7/solve.py
+++ 7/solve.py
@@ -11,50 +11,58 @@ from itertools import chain
 import pprint
 from copy import copy, deepcopy
 
-f = open("input.txt")
+def solvePart1():
+	f = open("input.txt")
 
-deps = defaultdict(set)
+	deps = defaultdict(set)
 
-taskOrder = ""
+	taskOrder = ""
 
-for line in f.readlines():
-	dependency = line[5]
-	dependee = line[36]
+	for line in f.readlines():
+		dependency = line[5]
+		dependee = line[36]
 
-	deps[dependee].add(dependency)
+		deps[dependee].add(dependency)
 
-mostRecentTaskWithLastDependencyRemoved = ''
+	mostRecentTaskWithLastDependencyRemoved = ''
 
-while deps:
-	allDependencies = set()
-	allDependees = set()
+	while deps:
+		allDependencies = set()
+		allDependees = set()
 
-	for k, v in deps.items():
-		allDependees = allDependees.union(k)
-		allDependencies = allDependencies.union(v)
+		for k, v in deps.items():
+			allDependees = allDependees.union(k)
+			allDependencies = allDependencies.union(v)
 
-	executableTasksSet = allDependencies - allDependees
-	executableTasks = list(executableTasksSet)
-	executableTasks.sort()
+		executableTasksSet = allDependencies - allDependees
+		executableTasks = list(executableTasksSet)
+		executableTasks.sort()
 
-	assert executableTasks, "No tasks! We expect to have some!"
+		assert executableTasks, "No tasks! We expect to have some!"
 
-	# note: only do the first task! not all! then
-	# we need to recalc what to do next.
-	taskOrder += executableTasks[0]
+		# note: only do the first task! not all! then
+		# we need to recalc what to do next.
+		taskOrder += executableTasks[0]
 
-	depsCopy = deepcopy(deps)
+		depsCopy = deepcopy(deps)
 
-	# remove task from all deps now it's done
-	for k, v in deps.items():
-		if executableTasks[0] in v:
-			depsCopy[k].remove(executableTasks[0])
-			if not depsCopy[k]:
-				del depsCopy[k]
-				mostRecentTaskWithLastDependencyRemoved = k
+		# remove task from all deps now it's done
+		for k, v in deps.items():
+			if executableTasks[0] in v:
+				depsCopy[k].remove(executableTasks[0])
+				if not depsCopy[k]:
+					del depsCopy[k]
+					mostRecentTaskWithLastDependencyRemoved = k
 
-	deps = depsCopy
+		deps = depsCopy
 
-taskOrder += mostRecentTaskWithLastDependencyRemoved
+	taskOrder += mostRecentTaskWithLastDependencyRemoved
+	return taskOrder
 
+def solvePart2(taskOrder):
+	print("part 2 todo")
+
+taskOrder = solvePart1()
 print('Part 1: ', taskOrder)
+
+solvePart2(taskOrder)
