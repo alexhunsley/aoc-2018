2cdaeb1a1e1918c3738c563258a929bcfae2a192
Hunsley.IO Ltd <alex.hunsley@gmail.com>
2018-12-09 13:48:45 +0000

    WIP scheduling

--- 7/solve.py
+++ 7/solve.py
@@ -22,24 +22,36 @@ from collections import Counter, defaultdict
 from itertools import chain
 import pprint
 from copy import copy, deepcopy
+from itertools import repeat
 
 # we have five workers (you + four elves).
-numWorkers = 5
+# numWorkers = 5
+numWorkers = 2
 
 WORKER_DATA_INDEX_TASK_ID = 0
 WORKER_DATA_INDEX_TIME_STARTED = 1
 WORKER_DATA_INDEX_TASK_DURATION = 2
 
+# 0 for the example, 60 for actual input data
+taskTimeExcessSeconds = 0
+
 # for each worker we store current task (or '.' for idle),
 # time task started (since second 0), and task duration
-workerData = [['.', -1, -1]] * numWorkers
+# workerData = list(repeat(['.', -1, -1], numWorkers)) 
+workerData = [['.', -1, -1] for i in range(0, numWorkers)]
+
+# print(workerData)
+# workerData[0][0] = 999
+# print(workerData)
+# sys.exit(1)
 
 # seonds since workers started making the sleigh
-secondsElapsed = 0
+currentTimeSeconds = 0
 
 tasksExecutable = set()
 
 def solvePart1():	
+	global tasksExecutable
 
 	# f = open("input.txt")
 	f = open("sampleInput.txt")
@@ -57,6 +69,7 @@ def solvePart1():
 	mostRecentTaskWithLastDependencyRemoved = ''
 
 	while deps:
+		print('start loop, workers =', workerData)
 		allDependencies = set()
 		allDependees = set()
 
@@ -64,27 +77,58 @@ def solvePart1():
 			allDependees = allDependees.union(k)
 			allDependencies = allDependencies.union(v)
 
-		executableTasksSet = allDependencies - allDependees
-		executableTasks = list(executableTasksSet)
-		executableTasks.sort()
+		newlyExecutableTasksSet = allDependencies - allDependees
 
-		assert executableTasks, "No tasks! We expect to have some!"
+		print('currently executable tasks: %s' % tasksExecutable)
+		print('newly executable tasks: %s' % newlyExecutableTasksSet)
+		tasksExecutable = tasksExecutable.union(newlyExecutableTasksSet)
+		print('hence, all tasks now executable = %s' % tasksExecutable)
+
+		tasksExecutableList = list(tasksExecutable)
+		tasksExecutableList.sort()
+
+		assert tasksExecutableList, "No task(s) startable! We expect to have at least one!"
+
+		# schedule as many tasks as we can - limited by tasks available for scheduling,
+		# and by amount of workers currently idle
+		idleWorkers = [worker for worker in workerData if worker[WORKER_DATA_INDEX_TASK_ID] == '.']
+
+		print('idle workers = ', idleWorkers)
+		for worker in idleWorkers:
+			taskId = tasksExecutableList.pop(0)
+			worker[WORKER_DATA_INDEX_TASK_ID] = taskId 
+			worker[WORKER_DATA_INDEX_TIME_STARTED] = currentTimeSeconds
+			worker[WORKER_DATA_INDEX_TASK_DURATION] = ord(taskId) - ord('A') + 1 + taskTimeExcessSeconds
+
+			print('made a new worker: ', worker)
+			if not tasksExecutableList:
+				print(' (no tasks left in exec list, so exiting scheduler)')
+				break
+
+		print('after scheduling all tasks I could, all workers = ', workerData)
+
+		# when is the nearest second time that one or more tasks complete?
+		
 
 		# note: only do the first task! not all! then
 		# we need to recalc what to do next.
-		taskOrder += executableTasks[0]
 
-		depsCopy = deepcopy(deps)
+		# taskOrder += tasksExecutableList[0]
 
-		# remove task from all deps now it's done
-		for k, v in deps.items():
-			if executableTasks[0] in v:
-				depsCopy[k].remove(executableTasks[0])
-				if not depsCopy[k]:
-					del depsCopy[k]
-					mostRecentTaskWithLastDependencyRemoved = k
+		# depsCopy = deepcopy(deps)
 
-		deps = depsCopy
+		# # remove task from all deps now it's done
+		# for k, v in deps.items():
+		# 	if tasksExecutableList[0] in v:
+		# 		depsCopy[k].remove(tasksExecutableList[0])
+		# 		if not depsCopy[k]:
+		# 			del depsCopy[k]
+		# 			mostRecentTaskWithLastDependencyRemoved = k
+
+		# deps = depsCopy
+		
+		# TEMP
+		break
 
 	taskOrder += mostRecentTaskWithLastDependencyRemoved
 	return taskOrder
