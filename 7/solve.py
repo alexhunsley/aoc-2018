7e1edca57ad12530fef6b113408d282c9fcccc6a
Hunsley.IO Ltd <alex.hunsley@gmail.com>
2018-12-09 11:05:31 +0000

    Changes before starting part 2 changes proper

--- 7/solve.py
+++ 7/solve.py
@@ -1,18 +1,48 @@
 # AOC2018 part 7
 #
 # Sleigh assembly:
-# plan:
+# Part 1 plan:
 #   Store a dict that maps a step to the step(s) that it depends on.
-#   'solve' the items that have no dependencies. Then remove their
-#   mentions as dependencies for anything else (honouring the alphabetical order).
-
+#   while (tasksRemaining):
+#      'solve' first item (alphabetically) that is executable (i.e. has no dependencies)
+#      Then remove its mentions as a dependency for anything else 
+#   (Hint: Items that have no dependencies appear in the dict value collections
+#   but are not anywhere as a dict key.)
+#
+# Part 2 plan:
+#   we maintain 'worker' details and the current time (seconds).
+#   while (tasksRemaining):
+#      schedule as many as possible of all task now executable (in alphabetical order).
+#      For the scheduled task(s) T that will next finish:
+#        mark each task as done
+#
+#
+#
 from collections import Counter, defaultdict
 from itertools import chain
 import pprint
 from copy import copy, deepcopy
 
-def solvePart1():
-	f = open("input.txt")
+# we have five workers (you + four elves).
+numWorkers = 5
+
+WORKER_DATA_INDEX_TASK_ID = 0
+WORKER_DATA_INDEX_TIME_STARTED = 1
+WORKER_DATA_INDEX_TASK_DURATION = 2
+
+# for each worker we store current task (or '.' for idle),
+# time task started (since second 0), and task duration
+workerData = [['.', -1, -1]] * numWorkers
+
+# seonds since workers started making the sleigh
+secondsElapsed = 0
+
+tasksExecutable = set()
+
+def solvePart1():	
+
+	# f = open("input.txt")
+	f = open("sampleInput.txt")
 
 	deps = defaultdict(set)
 
@@ -59,18 +89,6 @@ def solvePart1():
 	taskOrder += mostRecentTaskWithLastDependencyRemoved
 	return taskOrder
 
-def solvePart2(taskOrder):
-	print("part 2 todo")
-
-	# we have five workers (you + four elves).
-	# for each worker we store current task (or '.' for idle),
-	# and seconds remaining of task
-	workerData = [['.', 0]] * 5
-	# print(workerData)
-
-	# ah. we can't just use the order found for part 1.
-	# the time aspect means things can happen in a different order!	
 taskOrder = solvePart1()
 print('Part 1: ', taskOrder)
 
-solvePart2(taskOrder)
