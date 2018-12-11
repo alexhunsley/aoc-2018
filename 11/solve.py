5fe3788f0cd01536de18f6f1e67e9a1c387dedbc
Hunsley.IO Ltd <alex.hunsley@gmail.com>
2018-12-11 15:13:53 +0000

    WIP part 2 - sumlist move right, down working

--- 11/solve.py
+++ 11/solve.py
@@ -3,18 +3,132 @@
 
 # Input: 9221
 
+#
+# Part 2:
+#
+# More efficient square summing:
+# (yeah, we knew this was coming!)
+#
+# For square size S, maintain a horiz sum of S starting at some X.
+# To move right, subtract leftmost item, add rightmost. 
+# Likewise, for moving down, we have a list of these horizontal sums.
+# We chop off start of list and add to end to move downwards.
+#
+# Before doing a right move, copy the horiz sum list so we can restore later.
+#
+
 from collections import Counter, defaultdict
 from itertools import chain
 from pprint import pprint
 from copy import copy, deepcopy
 from itertools import repeat
 from functools import reduce
+import sys
+from operator import sub, add
+
+gridWidth = 300
+gridHeight = 300
 
 enableLog = True
 
 def logPrint(str):
 	if enableLog:
 		print(str, end='')
+
+# powergrid is a 2d array, addressed as powerGrid[y][x].
+# x, y are 1 based, as per the question
+#
+# returns a 'sumData': [sumList, x, y, squareSize]
+def makeSumList(x, y, squareSize, powerGrid):
+	# we want a list of sums, e.g. for squareSize = 3, we might have [3, 18, 9]
+	sums = [reduce(lambda x, y: x+y, powerGrid[y - 1][x-1:x-1+squareSize]) for y in range(y, y + squareSize)]
+	return [sums, x, y, squareSize]
+
+# update the given sumlist according to a right-wise move
+# gridDate is the power readings grid
+def moveSumListRight(sumData, gridData):
+	[sumList, x, y, squareSize] = sumData
+	print('got ', sumList, x, y, squareSize)
+	print('griddata = %s' % gridData)
+	newX = x + 1
+	print('new x = %d (%d)' % (newX, newX + squareSize - 1) )
+	if (newX + squareSize - 1) > gridWidth:
+		print('moveSumListRight: Got given an x too large for grid for a right move! x = %d' % x)
+		sys.exit(1)
+
+	newColumn = newX + squareSize - 1
+
+	print('y range = %s' % range(y - 1, y - 1 + squareSize))
+	print('newColumn = %d' % newColumn)
+	print('gridY = %s' % gridData[y])
+	# update sums
+	amountsToRemove = [gridData[yy][x - 1] for yy in range(y - 1, y - 1 + squareSize)]
+	print('amountsToRemove = %s' % amountsToRemove)
+	amountsToAdd = [gridData[yy][newColumn - 1] for yy in range(y - 1, y - 1 + squareSize)]
+	print('amountsToAdd = %s' % amountsToAdd)
+
+	sumList = map(sub, sumList, amountsToRemove)
+	sumList = list(map(add, sumList, amountsToAdd))
+	
+	print('new sumlist: %s' % sumList)
+
+	# if (y + squareSize - 1) > gridHeight:
+	# 	print('Got given an x too large for grid! y = %d' % y)
+	# 	sys.exit(1)
+
+
+def moveSumListDown(sumData, gridData):
+	[sumList, x, y, squareSize] = sumData
+	print('got ', sumList, x, y, squareSize)
+	print('griddata = %s' % gridData)
+	newY = y + 1
+	print('new y = %d (%d)' % (newY, newY + squareSize - 1) )
+	if (newY + squareSize - 1) > gridHeight:
+		print('moveSumListDown: Got given a y too large for grid for a down move! y = %d' % y)
+		sys.exit(1)
+
+	# remove sumlist item from start of list, add new one at the end
+	sumList = sumList[1:]
+
+	vals = gridData[y - 1 + squareSize][x-1:x-1+squareSize]
+	summy = reduce(lambda x, y: x+y, vals)
+	print('down move: vals, sum =%s, %s' % (vals, summy))
+
+	sumList.append(summy)
+	print(' after move down, sumlist = %s' % sumList)
+	
+def testSumLists():
+	sampleGrid = [[0,1,2],
+	              [3,4,5],
+	              [6,7,8]]
+
+	print(makeSumList(1, 1, 2, sampleGrid) )
+	print(makeSumList(2, 1, 2, sampleGrid) )
+	print(makeSumList(1, 2, 2, sampleGrid) )
+
+def testSumListsMove():
+	
+	sampleGrid4 = [[0,1,2,3],
+	              [4,5,6,7],
+	              [8,9,10,11],
+	              [12,13,14,15]]
+	#ORIG: 3, 5, 27
+	#new expected after right move: 6, 18, 30
+	s = makeSumList(1, 1, 3, sampleGrid4)
+	print(s)
+
+	# moveSumListRight(s, sampleGrid4)
+	moveSumListDown(s, sampleGrid4)
+
+# for testing
+gridWidth = 4
+gridHeight = 4
+testSumListsMove()
+
+# testSumLists()
+
+sys.exit(1)
+# reduce(lambda x, y: x+y, metadata)
 
 # serialNumber = 9221
 
@@ -35,8 +149,6 @@ def logPrint(str):
 
 serialNumber = 9221
 
-gridWidth = 300
-gridHeight = 300
 # gridWidth = 5
 # gridHeight = 5
 
@@ -93,7 +205,7 @@ for y in range(1, gridHeight - 1):
 		power = 0
 		for offsetX in range(0, 3):
 			for offsetY in range(0, 3):
-				# -1 for the 1 based stuff
+				# -1 for the 1 based stuff!
 				power += cols[y - 1 + offsetY][x - 1 + offsetX]
 		if power > maxPower:
 			maxPower = power
