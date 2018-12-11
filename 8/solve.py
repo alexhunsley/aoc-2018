fcd801543bedb006aa292a47116a402a878f8b5f
Hunsley.IO Ltd <alex.hunsley@gmail.com>
2018-12-11 11:23:53 +0000

    Remove debug logs

--- 8/solve.py
+++ 8/solve.py
@@ -4,13 +4,6 @@
 
 from functools import reduce
 
-enableLog = False
-
-def logPrint(str):
-	if enableLog:
-		print(str)
-
-
 f = open("input.txt")
 # f = open("exampleInput.txt")
 
@@ -50,7 +43,6 @@ idx = 2
 
 # node id, curr child being processed, num children, num metadata
 opStack.append([0, 0, numChildren, numMetadata])
-logPrint('stack initially = %s' % opStack)
 
 # map from child node ID to (parentNodeId, metadatas)
 nodeRelations = {}
@@ -64,10 +56,7 @@ cnt = 0
 nodeIdCounter = 0
 
 while True:
-	logPrint('start loop, i = %d, stack = %s' % (idx, opStack))
-
 	if not opStack:
-		logPrint(' stack now empty! finished')
 		break
 
 	numItemsOnStack = len(opStack)
@@ -84,11 +73,8 @@ while True:
 	# put modified data back on stack (currChild has incremented)
 	opStack.append((nodeId, currChild, numChildren, numMetadata))
 
-	logPrint(' stack not empty - doing children %d of %d' % (currChild, numChildren))
-
 	# if no children left, node children finished processing - process metadata and continue
 	if currChild == numChildren + 1:
-		logPrint('  .. reached end of (or no) children')
 		metaData = code[idx : idx + numMetadata]
 
 		debugTree += "%s <%s  %s\n" % (spacer, nodeId, metaData)
@@ -96,7 +82,6 @@ while True:
 		metaDataTotal += reduce(lambda x, y: x+y, metaData)
 
 		idx += numMetadata
-		logPrint(' nodeID: %s processed metadata %s, meta total: %d, stack idx now = %d ' % (nodeId, metaData, metaDataTotal, idx))
 		opStack.pop()
 
 		# if >0 items on stack, we have a parent node, identify it
@@ -105,12 +90,7 @@ while True:
 			parentNodeId = opStack[-1][0]
 
 		nodeRelations[nodeId] = [parentNodeId] + metaData
-
-		logPrint('      PARENT NODE: %s' % parentNodeId)
-
 		continue
-
-	logPrint(' processing CHILD %s' % currChild)
 
 	numChildren = code[idx]
 	numMetadata = code[idx + 1]
@@ -121,18 +101,14 @@ while True:
 	opStack.append([nodeIdCounter, 0, numChildren, numMetadata])
 
 
-
 def valueForNode(nodeId):
-	logPrint('=== ENTER value for node %s' % nodeId)
 	# nodes pointing to this as parent should
 	# be in correct order if sorted by nodeId
 
 	# TODO - we don't need the full dict I think, just the id
 	childNodes = {k: v for k, v in nodeRelations.items() if nodeId == v[0]}
-	logPrint('found child nodes %s' % childNodes)
 
 	metadata = nodeRelations[nodeId][1:]
-	logPrint('for that node, relations= %s' % nodeRelations[nodeId])
 
 	if len(childNodes) == 0:
 		return reduce(lambda x, y: x+y, metadata)
@@ -143,28 +119,14 @@ def valueForNode(nodeId):
 
 	total = 0
 	childNodeIdsThatExist = [nodeId for nodeId in metadata if nodeId <= len(childNodes)]
-	logPrint('childNodeIdsThatExist = %s' % childNodeIdsThatExist)
 	for nodeId in childNodeIdsThatExist:
-		logPrint('   ... calling sub childforNode... on id = %s' % nodeId)
-		# total += valueForNode(nodeId)
-
 		nthChildKey = list(childNodes.keys())[nodeId - 1]
-		logPrint('nth child key = %s' % nthChildKey)
 		total += valueForNode(nthChildKey)
-
-	logPrint('calc total = %s' % total)
-	logPrint('childNodeIdsThatExist=%s' % childNodeIdsThatExist)
-	logPrint('child nodes of %s are %s' % (nodeId, childNodes))
-	logPrint('child nodes sorted: %s' % childNodeIdsSorted)
 
 	return total
 
 with open("tree.txt", "w") as treefile:
     treefile.write(debugTree)
-
-logPrint('relations: %s' % nodeRelations)
-
-logPrint(opStack)
 
 print(' Part 1: metadata total = ', metaDataTotal)
 print(' Part 2: node(0) value: %s' % valueForNode(0))
