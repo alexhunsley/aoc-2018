e8829bb7fde4010f3817e8a4f8b63f706f2238b5
Hunsley.IO Ltd <alex.hunsley@gmail.com>
2018-12-15 10:29:28 +0000

    Reading all symbols from cart input now

--- 13/solve.py
+++ 13/solve.py
@@ -1,7 +1,13 @@
 # solve.py 13
 #
+# We are using 0 based coords internally.
+#
 # do carts always appear on straight sections in the input? I assume so.
-# so on input can always replace v, ^ with | and <, > with -
+#
+# Observation: if we store all +, / and \ locations, we can disregard 
+# the straight section markers | and -. So more efficient sparse 
+# storage of where the former 3 items occur.
+#
 
 from collections import Counter, defaultdict
 from itertools import chain
@@ -9,6 +15,7 @@ import pprint
 from copy import copy, deepcopy
 from itertools import repeat
 from functools import reduce
+
 
 enableLog = True
 
@@ -19,16 +26,57 @@ def logPrint(str):
 # f = open("input.txt")
 f = open("shortExample.txt")
 
-lines = [x.strip() for x in f.readlines()]
+lines = f.readlines()
+# lines = [x.strip() for x in f.readlines()]
 
 carts = []
 
+cartStartsToVelocities = {'>': ( 1,  0),
+					      '<': (-1,  0),
+					      '^': ( 1, -1),
+					      'v': ( 1,  1)
+}
+
+crossingLocs = []
+slashLocs = []
+bslashLocs = []
+
 lineIndex = 0
 for l in lines:
-	print(l)
-	# find all occurence of this char
-	rightCartXPositions = [pos for pos, char in enumerate(l) if char == '>']
-	for cartXPos in rightCartXPositions:
-		carts.append([(cartXPos, lineIndex), (1, 0)])
+	print("%s" % l, end='')
 
-	print("line %s, r carts = %s" % (l, carts))
+	# find all occurence of cart chars
+	for (cartChar, startVel) in cartStartsToVelocities.items():
+		cartXPositions = [pos for pos, char in enumerate(l) if char == cartChar]
+		for cartXPos in cartXPositions:
+			carts.append([(cartXPos, lineIndex), startVel])
+
+	cartXPositions = [pos for pos, char in enumerate(l) if char == cartChar]
+	for cartXPos in cartXPositions:
+		carts.append([(cartXPos, lineIndex), startVel])
+
+	# read +
+	crossingPositions = [pos for pos, char in enumerate(l) if char == '+']
+	for crossPos in crossingPositions:
+		crossingLocs.append([(crossPos, lineIndex)])
+
+
+	# read /
+	slashPositions = [pos for pos, char in enumerate(l) if char == '/']
+	for slashPos in slashPositions:
+		slashLocs.append([(slashPos, lineIndex)])
+
+	# read \
+	bslashPositions = [pos for pos, char in enumerate(l) if char == '\\']
+	for bslashPos in bslashPositions:
+		bslashLocs.append([(bslashPos, lineIndex)])
+
+	lineIndex += 1
+
+print('\n====end')
+print("carts = %s" % carts)
+print("crossLocs = %s" % crossingLocs)
+print("slashLocs = %s" % slashLocs)
+print("bslashLocs = %s" % bslashLocs)
+
+
