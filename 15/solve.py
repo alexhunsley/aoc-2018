fa01dc7d21c5a4c7a2cb0e6e97e315d390d32448
Hunsley.IO Ltd <alex.hunsley@gmail.com>
2018-12-17 17:24:51 +0000

    Add test of shortest distance - working

--- 15/solve.py
+++ 15/solve.py
@@ -16,9 +16,9 @@ def logPrint(str):
 	if enableLog:
 		print(str, end='')
 
-f = open("input.txt")
+# f = open("input.txt")
+f = open("testShortestDistInput.txt")
 
-map = []
 mobs = []
 mobChars = ['E', 'G']
 
@@ -26,18 +26,18 @@ lines = f.readlines()
 
 mapSize = (len(lines[0].rstrip("\r\n")), len(lines))
 
-for lineIndex, line in enumerate(f.readlines()):
+for lineIndex, line in enumerate(lines):
 	line = line.strip()
 
 	plainLine = line
 	for mobChar in mobChars:
 		plainLine = plainLine.replace(mobChar, '.')
 		mobPositions = [pos for pos, char in enumerate(line) if char == mobChar]
-
+		print('mob pos', mobPositions)
 		for m in mobPositions:
 			m = SimpleNamespace(kind = mobChar, pos = [m, lineIndex])
 			mobs.append(m)
-	print(plainLine)
+	# print(plainLine)
 				# c = SimpleNamespace(pos=[cartXPos, lineIndex], vel=startVel, behavIndex=CrossingBehaviour.TURNLEFT, processed=False, id=cartIndex)
 
 
@@ -55,10 +55,60 @@ def printMap():
 
 		print('')
 
-print('mobs = ', mobs)
+def printMapWithDists():
+	mobInfo = [ [m.pos, m.kind] for n in mobs ]
+	
+	for y in range (0, mapSize[1]):
+		for x in range (0, mapSize[0]):
+			if (x, y) in shortestDistMap:
+				print('%s' % repr(shortestDistMap[(x, y)]).ljust(3), end='')
+			else:
+				print('%s  ' % lines[y][x], end='')
+
+		print('')
+
+def sortedMobs():
+	# left to right, top to bottom order (i.e. reading order)
+	return sorted(mobs, key=lambda m: m.pos[0] + m.pos[1] * mapSize[0])
+
+# while (any targets remain):
+#  each unit turn:
+#   in range?
+#.    attack
+#.  else
+#.    any open squares NSEW of target?
+#.      move there
+#     else
+#       end turn
+
 printMap()
+# print('mobs = ', mobs)
 
+print('sroted mobs =', sortedMobs())
 
+shortestDistMap = {}
+# calcShortestDist([9, 4], [7, 7], 0)
+
+neighbourCells = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+def calcShortestDist(a, distAccum):
+	print('calc shortest dist: a = ', a, ' accum = ', distAccum)
+	if (not a in shortestDistMap or shortestDistMap[a] > distAccum):
+		shortestDistMap[a] = distAccum
+	else:
+		return
+
+	for cell in neighbourCells:
+		c = [a[0] + cell[0], a[1] + cell[1]]
+		print('calc c=', c)
+		if lines[ c[1] ][ c[0] ] == '.':
+			calcShortestDist((c[0], c[1]), distAccum + 1)
+
+calcShortestDist((8, 3), 0)
+
+print('dists: ', shortestDistMap)
+
+printMapWithDists()
 # 	x = line[10:16].strip()
 # 	y = line[17:24].strip()
 	
