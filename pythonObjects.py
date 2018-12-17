e8127ff16c1ab4db1e4f389c4c271f7e5d464b8e
Hunsley.IO Ltd <alex.hunsley@gmail.com>
2018-12-17 13:52:11 +0000

    Update python notes

--- pythonObjects.py
+++ pythonObjects.py
@@ -33,7 +33,7 @@ print('Plain class. a == b: %s' % (a == b))
 # Can pass arbitrary stuff into init.
 # Nice repr ('eval(repr)' makes the object).
 # equality defined based on properties dict.
-# 
+# BUT it is possible to 'half make' an object, and delete its properties!
 a = SimpleNamespace()
 a.age = 7
 a.name = 'Alex'
@@ -45,12 +45,21 @@ print('SimpleNamespace: a == b: %s' % (a == b))
 
 ##########################################################
 # namedtuple
+# Much like SimpleNamespace, but more structured:
+# You have to make complete objects, and you cannot delete its properties!
+# Also it is immutable.
+#
+# So nametuple will fail earlier than SimpleNamespace, basically.
+
 
 Point = namedtuple('Point', ['x', 'y'])
 
 p = Point(11, y=22)
 # readable repr
 print('namedtuple: p =', p)
+
+# this would not be allowed! immutable.
+# p.x = 5
 
 t=eval(repr(p))
 print('made t from repr:', t)
